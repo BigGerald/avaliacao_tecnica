16737252c36051eaca6e0167125fb5502cbdbc9b
José Geraldo Duarte Júnior <117238608+BigGerald@users.noreply.github.com>
2022-11-02 16:03:45 -0300

    Add files via upload

--- PraticaJS-JoseGeraldoDuarteJunior.js
+++ PraticaJS-JoseGeraldoDuarteJunior.js
@@ -8,43 +8,43 @@ const pessoas = [
   { name: "Antonio Miguel", age: 69 },
 ];
 //----------------Exercicio 3----------------
-const ex3 = pessoas.map((user) => {
-  if (user.name == "Gabriel Gomes") {
-    return user;
+const ex3 = pessoas.map((item) => {
+  if (item.name == "Gabriel Gomes") {
+    return item;
   }
 });
 console.log(ex3);
 
 //----------------Exercicio 4----------------
-const ex4 = pessoas.map((user) => {
-  return user.name;
+const ex4 = pessoas.map((item) => {
+  return item.name;
 });
 console.log(ex4);
 
-const ex4Bonus = pessoas.map((user) => {
-  return user.name.split(" ",1);
+const ex4Bonus = pessoas.map((item) => {
+  return item.name.split(" ",1);
 });
 console.log(ex4Bonus);
 
 //----------------Exercicio 5----------------
-const ex5 = pessoas.map((user, index) => {
-  user.id = index + 1;
-  return { id: user.id, name: user.name, age: user.age };
+const ex5 = pessoas.map((item, index) => {
+  item.id = index + 1;
+  return { id: item.id, name: item.name, age: item.age };
 });
 console.log(ex5);
 
 //----------------Exercicio 6----------------
-const ex6 = ex5.map((user) => {
-  if (user.age >= 18) {
-    return user;
+const ex6 = ex5.map((item) => {
+  if (item.age >= 18) {
+    return item;
   }
 });
 console.log(ex6);
 
 //----------------Exercicio 7----------------
 let ex7 = 0;
-ex5.forEach((user) => {
-  ex7 = user.age + ex7;
+ex5.forEach((item) => {
+  ex7 = item.age + ex7;
 });
 ex7 = ex7 / pessoas.length;
 
